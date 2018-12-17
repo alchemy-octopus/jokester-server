ac59cabbc27340d7683c4fa52557e76b136681c5
JeiWolfei <jvampy22@gmail.com>
2018-12-17 14:29:45 -0800

    tables working

--- scripts/create-tables.js
+++ scripts/create-tables.js
@@ -10,13 +10,14 @@ client.query(`
   CREATE TABLE IF NOT EXISTS jokes (
     id SERIAL PRIMARY KEY,
     title VARCHAR(256) NOT NULL,
+    source VARCHAR(256) NOT NULL,
     profile_id INTEGER NOT NULL REFERENCES profiles(id)
   );
   CREATE TABLE IF NOT EXISTS ratings (
     id SERIAL PRIMARY KEY,
-    score INTEGER,
-    count INTEGER,
-    joke_id INTEGER NOT NULL REFERENCES jokes(id)
+    score INTEGER, 
+    joke_id INTEGER NOT NULL REFERENCES jokes(id),
+    profile_id INTEGER NOT NULL REFERENCES profiles(id)
   );
 `)
   .then(
--- scripts/load-seed-data.js
+++ scripts/load-seed-data.js
@@ -3,15 +3,15 @@ const client = require('../lib/db-client');
 const bcrypt = require('bcryptjs');
 
 const jokes = [
-  { title: 'What to you get when you cross the Atlantic with the Titanic? About half way' },
-  { title: 'What did one wall say to the other wall? Lets meet at the corner' },
-  { title: 'Why did Cleopatra fall off the swing? Because she is dead' }
+  { title: 'What to you get when you cross the Atlantic with the Titanic? About half way', source:'search' },
+  { title: 'What did one wall say to the other wall? Lets meet at the corner', source:'search' },
+  { title: 'Why did Cleopatra fall off the swing? Because she is dead', source: 'custom' }
 ];
 
 const ratings = [
-  { score: 8, count: 2 },
-  { score: 4, count: 1 },
-  { score: 9, count: 3 }
+  { score: 1 },
+  { score: 3 },
+  { score: 4 }
 ];
 
 client.query(`
@@ -22,6 +22,8 @@ client.query(`
 ['jei', bcrypt.hashSync('jeijei', 8)]
 )
   .then(result => {
+    const profile = result.rows[0];
+
 
     return Promise.all(
       jokes.map(jokes => {
@@ -29,19 +31,20 @@ client.query(`
         INSERT INTO goals (title, profiles_id)
         VALUES ($1, $2)
       `,
-        [jokes.title, profiles.id]);
+        [jokes.title, profile.id]);
       })
     );
   })
   .then(result => {
+    const profile = result.rows[0];
 
     return Promise.all(
-      ratings.map(ratings => {
+      ratings.map(rating => {
         return client.query(`
-        INSERT INTO ratings (score, count, jokes_id)
+        INSERT INTO ratings (score, jokes_id, profiles_id)
         VALUES ($1, $2, $3)
       `,
-        [ratings.score, ratings.count, profiles.id]);
+        [rating.score, jokes.id, profile.id]);
       })
     );
   })
